fdca2b3e9fa5ecf9f6d80a5ca4644e036c1cfd47
Dilnaz Zhanai <dilkawai07@gmail.com>
2026-02-02 20:19:11 +0600

    practice

---
+++ practice_2/21.py
@@ -0,0 +1,11 @@
+score=int(input())
+if score >=90 :
+    print("Grade: A")
+if score >=80:
+    print("Grade: B")
+if score >=70:
+    print("Grade: C")
+if score >=60:
+    print("Grade: D")
+else :
+    print("Grade: F")
